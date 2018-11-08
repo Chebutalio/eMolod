863381e8ea30eda9b982e41466e05220cf1c8896
Chebutalio <43986450+Chebutalio@users.noreply.github.com>
2018-11-08 14:15:07 +0200

    Update homework4_3.js

--- homework4_3.js
+++ homework4_3.js
@@ -2,12 +2,13 @@ var arr = [1, 24, 22, 11, 23, 13, 23, 63, 85];
 
 Array.prototype.mySort = function() {
 	var done = false;
+	var elem = 0;
 	while (!done) {
 		done = true;
 		for (var i = 1; i < this.length; i++) {
 			if (this[i - 1] > this[i]) {
 				done = false;
-				var elem = this[i - 1];
+				elem = this[i - 1];
 				this[i - 1] = this [i];
 				this[i] = elem;
 			}
